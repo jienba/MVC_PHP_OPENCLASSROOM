aec1d50efb84f4f696d11226e8c4cbdd80f83163
jienba <adamjienba@outlook.fr>
2021-08-25 19:30:48 +0000

    MVC folder work 😃😃😃!

--- mvcOpennClassroom/3rd_TP_Blog/index.php
+++ mvcOpennClassroom/3rd_TP_Blog/index.php
@@ -1,4 +1,21 @@
 <?php
-require('model.php');
-$req = getPosts();
-require('indexView.php');
+require "controller/frontend.php";
+
+
+if (isset($_GET['action'])){
+    if ($_GET['action'] == 'listPosts'){
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post'){
+        if (isset($_GET['id']) && $_GET['id'] > 0 ){
+            post();
+        }
+    }
+    else {
+        echo 'Erreur: Aucun identifiant de billet envoyé';
+    }
+
+}
+else{
+    listPosts();
+}
